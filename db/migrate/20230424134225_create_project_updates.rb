2048b3bd1f5f1f1d8bf02e88c13102a94f225f7b
fabio-str <fabio93straessle@hotmail.com>
2023-04-24 14:45:17 +0100

    database migrations

---
+++ db/migrate/20230424134225_create_project_updates.rb
@@ -0,0 +1,11 @@
+class CreateProjectUpdates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_updates do |t|
+      t.string :title
+      t.string :content
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/project_updates.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :project_update do
+    title { "MyString" }
+    content { "MyString" }
+    project { nil }
+  end
+end
