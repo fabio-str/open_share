2455564dd04d6883f59e3f3265863641882fb4dc
fabio-str <fabio93straessle@hotmail.com>
2023-05-02 17:29:06 +0100

    some branding and styling adaptations

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,10 +12,10 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: { // custom color palette for branding
-        'primary': 'rgb(79, 70, 229)',
+        'primary': '#a22317',
         'primary-hover': {
-          '700': '#a4411c',
-          '500': '#EA9A72'
+          '700': '#922015',
+          '500': '#922015'
         }
       }
     },
