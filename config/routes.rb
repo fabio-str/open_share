6da241132285cee9b6307852145653dcee1f1f34
fabio-str <fabio93straessle@hotmail.com>
2023-05-02 13:07:08 +0100

    Created admin panel + heading partial for pages

--- config/routes.rb
+++ config/routes.rb
@@ -23,15 +23,20 @@ Rails.application.routes.draw do
     get "/#{page}", to: "pages##{page}", as: "#{page.gsub('-', '_')}"
   end
 
-  # admin panels
-  authenticated :user, -> user { user.admin? } do
-    namespace :admin do
-      resources :dashboard, only: [:index]
-      resources :impersonations, only: [:new]
-      resources :users, only: [:edit, :update, :destroy]
-    end
-
-    # convenience helper
-    get 'admin', to: 'admin/dashboard#index'
+  namespace :admin do
+    get '/', to: 'pages#dashboard'
   end
+  
+
+  # admin panels
+  # authenticated :user, -> user { user.admin? } do
+  #   namespace :admin do
+  #     resources :dashboard, only: [:index]
+  #     resources :impersonations, only: [:new]
+  #     resources :users, only: [:edit, :update, :destroy]
+  #   end
+
+  #   # convenience helper
+  #   get 'admin', to: 'admin/dashboard#index'
+  # end
 end
